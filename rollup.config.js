92a12e3f15a3c282ea93dd0f63d0d012691636c9
Tadeusz Łazurski <tadeusz@lazurski.pl>
2018-02-25 22:31:39 +0100

    Setup rollup to compile and serve the app from Coffee Script entry point
    Files to be compiled are in src/ directory. Static assets (currently it
    is only index.html) in assets/.
    
    For Elm plugin babel-runtime had to be added (see
    https://github.com/ulisses-alves/rollup-plugin-elm/issues/1).
    
    The develop script can be used to start a development server (watch +
    serve).

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import commonjs from 'rollup-plugin-commonjs'
+import elm from 'rollup-plugin-elm'
+import coffeescript from 'rollup-plugin-coffee-script'
+import serve from "rollup-plugin-serve"
+
+export default {
+  input: 'src/index.coffee',
+  output: {
+    file: `built/bundle.js`,
+    format: 'iife'
+  },
+  plugins: [
+    serve({
+      contentBase: ["built", "assets"]
+    }),
+    coffeescript(),
+    elm({
+        exclude: 'elm_stuff/**',
+        compiler: {
+          debug: true
+        }
+    }),
+    commonjs({
+      // add .elm extension
+      extensions: ['.js', '.elm']
+    })
+  ],
+  watch: {
+    // add .elm files to watched files
+    include: 'src/**'
+  }
+}
